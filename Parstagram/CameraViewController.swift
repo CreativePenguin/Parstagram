e075a434c489d402aa5f5b72e58e347c4d7d27df
Winston Peng <panda501wp@gmail.com>
2022-03-08 22:03:54 -0500

    can now save images and comments
    I saved the world Dawn

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -6,16 +6,66 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var commentField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func onSubmitButton(_ sender: Any) {
+        let post = PFObject(className: "Posts")
+        
+        post["caption"] = commentField.text
+        post["author"] = PFUser.current()!
+        
+        let imageData = imageView.image!.pngData()
+        let file = PFFileObject(data: imageData!)
+        
+        post["image"] = file
+        
+        post.saveInBackground { success, error in
+            if success {
+                self.dismiss(animated: true, completion: nil)
+                print("comment saved!")
+            } else {
+                print("failed to save comment")
+                print("Error: \(error!.localizedDescription)")
+            }
+        }
+    }
+    
+    @IBAction func onCameraButton(_ sender: Any) {
+        print("click registered")
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 300, height: 300)
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        imageView.image = scaledImage
+        
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
